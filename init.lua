20b3ab54340c36057c91526ddd1e78f3d65eb7b3
samaphp <samaphp@users.noreply.github.com>
2024-10-13 09:08:05 +0300

    use zsh by default and add shortcuts for floaterm

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_keymap('n', '<C-t>', ':FloatermToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
+vim.g.floaterm_shell = 'zsh'
