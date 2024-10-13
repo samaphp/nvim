7c09ed42662a288a5892efa09c84afd77332cb7a
samaphp <samaphp@users.noreply.github.com>
2024-10-13 09:23:56 +0300

    add shortcut to open terminal

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.g.floaterm_shell = 'zsh'
 vim.api.nvim_set_keymap('n', '<C-t>', ':FloatermToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
-vim.g.floaterm_shell = 'zsh'
+vim.api.nvim_set_keymap('n', '<C-A-t>', ':FloatermNew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-A-t>', '<C-\\><C-n>:FloatermNew<CR>', { noremap = true, silent = true })
